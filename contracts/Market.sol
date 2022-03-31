c69a8e5d378240bfdfb025b162868b9a5592f394
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-31 22:06:49 +0100

    market init

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Market is ReentrancyGuard {
-    using Counters for Counters.Counter;
-    Counters.Counter private _itemsId;
-    Counters.Counter private _itemsSold;
-
     address payable public owner;
     uint256 public listingPrice = 0.015 ether;
 
