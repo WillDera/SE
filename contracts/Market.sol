bd6c4820a65dabc80bd347178869d91e7eaa81a7
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-04-02 22:18:20 +0100

    create market item

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Market is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+
     address payable public owner;
     uint256 public listingPrice = 0.015 ether;
 
@@ -13,7 +16,68 @@ contract Market is ReentrancyGuard {
         owner = payable(msg.sender);
     }
 
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract; // nft contract address
+        uint256 tokenId; // nft id
+        address payable seller; // seller address
+        address payable owner; // buyer address
+        uint256 price; // listing price of nft
+        bool sold; // sale state of nft. true = sold / false = not sold
+    }
+
+    event MarketItemCreated(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
+    }
+
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be atleast 1 wei");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        // transfer ownership of the token
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        // emit token creation event
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
     }
 }
